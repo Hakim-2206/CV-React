3fa4d8b74d8e9c4b572a96d58a986243bedaaea8
Hakim-2206 <hakim.hakim2206@gmail.com>
2024-10-31 14:43:46 +0100

    Ajout experiences

--- src/datas/Experiences.js
+++ src/datas/Experiences.js
@@ -1,25 +1,33 @@
 const DataExperiences = [
     {
         id: 1,
+        title:"OP CONSULTING - Développeur informatique(Alternance)",
+        date:"Janvier 2023 - Août 2023",
+        location:"Full Remote",
+        text: 'Création et maintenance de sites web esthétiques et intuitifs, développement de fonctionnalités, résolution de problèmes techniques, mise en place de solutions. Gestion de bases de données. Déploiements en production (OVH, IONOS.)'
+    },
+    {
+        id: 2,
         title:"C.R.E.C.I - Assistant en éléctricité",
         date:"Octobre 2021 - Mai 2022",
         location:"Nice, France",
         text: 'Aider le personnel spécialisé en électricité à monter, à entretenir, et à réparer des équipements électriques comme les appareils d\'éclairage dans un établissement commercial, industriel ou autre'
     },
     {
-        id: 2,
+        id: 3,
         title:"G.S.A France & Protector - Agent S.S.I.A.P 1",
         date:"Mars 2019 - Janvier 2021",
         location:"Nice, France",
         text: 'Rondes de sécurité, gardiennage, premiers secours aux blessés, ouverture et fermeture du site, gestions des clefs, gestion des conflits.'
     },
     {
-        id: 3,
+        id: 4,
         title:"SECURITAS France - Agent de sûreté aéroportuaire",
         date:"Juin 2018 - Septembre 2018",
         location:"Camas Formation, NICE",
         text: 'Vérifier les effets personnels sur le tapis de la machine à rayons X, effectuer la palpation de sécurité, contrôler les personnes à l\'aide du magnétomètre et fouille éventuelle des bagages cabine'
     },
+    
 
 ]
 
--- src/datas/Formations.js
+++ src/datas/Formations.js
@@ -1,18 +1,24 @@
 const DataFormations = [
     {
         id: 1,
-        title:"Formation Développeur Intégrateur Web",
-        date:"2022",
+        title:"Formation Full SDtack Developer - Spécialisation React/Symfony",
+        date:"2023",
         location:"3W Academy (télé présentiel)"
     },
     {
         id: 2,
+        title:"Formation Développeur Intégrateur Web -  HTML - CSS - JS - PHP - Normes W3C, Programmation orientée objet, Architecture MVC, SGBD",
+        date:"2022",
+        location:"3W Academy (télé présentiel)"
+    },
+    {
+        id: 3,
         title:"Formation Agent de sûreté aeroportuaire",
         date:"2018",
         location:"Camas Formation, NICE"
     },
     {
-        id: 3,
+        id: 4,
         title:"Formation Agent de sécurité incendie (S.S.I.A.P 1)",
         date:"2017",
         location:"Camas Formation, NICE"
